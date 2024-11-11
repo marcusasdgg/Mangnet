ffb16e760d9db6f3d9cd06c52f4d2b377c297e76
marcusasdgg <marcusolachan@icloud.com>
2024-11-12 02:37:29 +1100

    feat: added rudiemntary support for manganato

---
+++ app/src/main/java/com/example/poomagnet/ui/SettingsScreen/SettingsViewModel.kt
@@ -0,0 +1,18 @@
+package com.example.poomagnet.ui.SettingsScreen
+
+import android.util.Log
+import androidx.lifecycle.ViewModel
+import com.example.poomagnet.mangaRepositoryManager.MangaRepositoryManager
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class SettingsViewModel @Inject constructor(
+    private val repo: MangaRepositoryManager
+) : ViewModel() {
+    fun getBackUp(): String {
+        val s =  repo.getBackUpFromFile()
+        Log.d("TAG", "getBackUp: $s")
+        return s
+    }
+}
